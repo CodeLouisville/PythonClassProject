c109e1735606f6934ff90ddbcbb8ce02bac11dcf
sschwein <thatguysch@gmail.com>
2017-10-12 12:24:54 -0400

    setting.py update

--- louie_pizza/louie_pizza/settings.py
+++ louie_pizza/louie_pizza/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'louie_pizza.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
